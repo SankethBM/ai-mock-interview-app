040d71d861477712604cbfb8d1cdb574c29d2037
Sanketh B M <sankethbm333@gmail.com>
2026-04-13 01:10:47 +0530

    landing page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      Hello
-      <Button>Button</Button>
+    <div className="min-h-screen bg-[linear-gradient(to_bottom_right,white,#d1d5db,white,#d1d5db)]  ">
+      {/* Header Section */}  
+      <Header/>
+
+      {/* Hero Section */}
+      <Hero/>
     </div>
 
   );  
